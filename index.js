e2059e45ba122416cc608e7eb5c53377ca7d4242
Tiago Cardoso <tbcardoso@outlook.com>
2017-05-30 15:22:07 +0100

    Configure eslint and fix problems found

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var ftp2gdrive = require('./src/webtask');
 
 var secrets = ini.parse(fs.readFileSync('./config/secrets.txt', 'utf-8'));
 
-ftp2gdrive({secrets: secrets}, function(){});
+ftp2gdrive({secrets: secrets}, function() {});
--- src/webtask.js
+++ src/webtask.js
@@ -1,24 +1,23 @@
 module.exports = function(ctx, cb) {
+	var Client = require('ssh2').Client;
 
-  var Client = require('ssh2').Client;
+	var conn = new Client();
+	conn.on('ready', function() {
+		console.log('Client :: ready');
+		conn.sftp(function(err, sftp) {
+			if (err) throw err;
+			sftp.readdir(ctx.secrets.FTP_PATH, function(err, list) {
+				if (err) throw err;
+				console.dir(list);
+				conn.end();
+			});
+		});
+	}).connect({
+		host: ctx.secrets.FTP_HOST,
+		port: ctx.secrets.FTP_PORT,
+		username: ctx.secrets.FTP_USER,
+		password: ctx.secrets.FTP_PASSWORD,
+	});
 
-  var conn = new Client();
-  conn.on('ready', function() {
-    console.log('Client :: ready');
-    conn.sftp(function(err, sftp) {
-      if (err) throw err;
-      sftp.readdir(ctx.secrets.FTP_PATH, function(err, list) {
-        if (err) throw err;
-        console.dir(list);
-        conn.end();
-      });
-    });
-  }).connect({
-    host: ctx.secrets.FTP_HOST,
-    port: ctx.secrets.FTP_PORT,
-    username: ctx.secrets.FTP_USER,
-    password: ctx.secrets.FTP_PASSWORD,
-  });
-
-  cb(null, { result: 'success' });
+	cb(null, { result: 'success' });
 };
