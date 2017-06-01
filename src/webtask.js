95efe9bb1ca090ef5e0a0ca94fe2f21136a1e03b
Tiago Cardoso <tbcardoso@outlook.com>
2017-06-01 16:48:09 +0100

    Add google drive API dependencies and configuration

--- src/webtask.js
+++ src/webtask.js
@@ -1,5 +1,20 @@
+var Client = require('ssh2').Client;
+var google = require('googleapis');
+
 module.exports = function(ctx, cb) {
-	var Client = require('ssh2').Client;
+	var OAuth2 = google.auth.OAuth2;
+
+	var oauth2Client = new OAuth2(
+		ctx.secrets.GOOGLE_CLIENT_ID,
+		ctx.secrets.GOOGLE_CLIENT_SECRET
+	);
+
+	oauth2Client.setCredentials({
+		//access_token: '',
+		refresh_token: ctx.secrets.GOOGLE_REFRESH_TOKEN
+	});
+
+	listFiles(oauth2Client);
 
 	var conn = new Client();
 	conn.on('ready', function() {
@@ -21,3 +36,27 @@ module.exports = function(ctx, cb) {
 
 	cb(null, { result: 'success' });
 };
+
+function listFiles(auth) {
+	var service = google.drive('v3');
+	service.files.list({
+		auth: auth,
+		pageSize: 10,
+		fields: 'nextPageToken, files(id, name)'
+	}, function(err, response) {
+		if (err) {
+			console.log('The API returned an error: ' + err);
+			return;
+		}
+		var files = response.files;
+		if (files.length === 0) {
+			console.log('No files found.');
+		} else {
+			console.log('Files:');
+			for (var i = 0; i < files.length; i++) {
+				var file = files[i];
+				console.log('%s (%s)', file.name, file.id);
+			}
+		}
+	});
+}
