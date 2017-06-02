08f7ca50ccc4200f8a7017c4ec2283951d4f6cdc
Tiago Cardoso <tbcardoso@outlook.com>
2017-06-02 15:26:00 +0100

    Add code to backup the file from FTP to Google Drive

--- src/webtask.js
+++ src/webtask.js
@@ -2,6 +2,7 @@
 
 const SSH2Client = require('ssh2').Client;
 const google = require('googleapis');
+const dateFormat = require('dateformat');
 
 module.exports = function(ctx, cb) {
 	const OAuth2 = google.auth.OAuth2;
@@ -17,8 +18,6 @@ module.exports = function(ctx, cb) {
 	});
 
 	const drive = google.drive('v3');
-	listFiles(drive, oauth2Client, ctx.secrets.DRIVE_BACKUP_FOLDER_ID);
-	uploadFile(drive, oauth2Client, ctx.secrets.DRIVE_BACKUP_FOLDER_ID);
 
 	const ssh2Conn = new SSH2Client();
 	ssh2Conn.on('ready', function() {
@@ -26,11 +25,13 @@ module.exports = function(ctx, cb) {
 
 		ssh2Conn.sftp(function(err, sftp) {
 			if (err) throw err;
-			sftp.readdir(ctx.secrets.FTP_PATH, function(err, list) {
-				if (err) throw err;
-				console.dir(list);
-				ssh2Conn.end();
-			});
+
+			const filePath = ctx.secrets.FTP_PATH;
+			uploadFile(drive, oauth2Client, ctx.secrets.DRIVE_BACKUP_FOLDER_ID,
+				getFileName(filePath, new Date()),
+				sftp.createReadStream(filePath), function() {
+					ssh2Conn.end();
+				});
 		});
 	}).connect({
 		host: ctx.secrets.FTP_HOST,
@@ -39,41 +40,22 @@ module.exports = function(ctx, cb) {
 		password: ctx.secrets.FTP_PASSWORD,
 	});
 
-	cb(null, { result: 'success' });
+	cb(null, { result: 'IN_PROCESS' });
 };
 
-function listFiles(drive, auth, folderId) {
-	drive.files.list({
-		auth: auth,
-		q: "'" + folderId + "'" + ' in parents',
-		pageSize: 10,
-		fields: 'nextPageToken, files(id, name)'
-	}, function(err, response) {
-		if (err) {
-			console.log('The API returned an error: ' + err);
-			return;
-		}
-		const files = response.files;
-		if (files.length === 0) {
-			console.log('No files found.');
-		} else {
-			console.log('Files:');
-			for (let i = 0; i < files.length; i++) {
-				const file = files[i];
-				console.log('%s (%s)', file.name, file.id);
-			}
-		}
-	});
+function getFileName(filePath, date) {
+	return (dateFormat(date, 'yyyymmddHHMMss') + '_' +
+		filePath.substring(filePath.lastIndexOf('/') + 1));
 }
 
-function uploadFile(drive, auth, folderId) {
+function uploadFile(drive, auth, folderId, fileName, fileStream, callback) {
 	var fileMetadata = {
-		name: 'test.txt',
+		name: fileName,
 		parents: [folderId]
 	};
 	var media = {
 		//mimeType: 'text/plain',
-		body: 'hello...'
+		body: fileStream//'hello...'
 	};
 	drive.files.create({
 		auth: auth,
@@ -82,11 +64,10 @@ function uploadFile(drive, auth, folderId) {
 		fields: 'id'
 	}, function(err, file) {
 		if (err) {
-			// Handle error
-			console.log(err);
+			throw err;
 		} else {
 			console.log('File Id: ', file.id);
+			callback();
 		}
 	});
-
 }
